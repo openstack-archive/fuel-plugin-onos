5038c55a6e3cc562af33a83bf86bea1531e66014
xuchao <xu.chao@99cloud.net>
2015-11-24 12:40:54 +0800

    Update neutron ml2_plugin onos metadata to neutron.pp
    Change-Id: I638e5bf0b58bde1090dc9a3739f8f7af83fb049c

--- deployment_scripts/puppet/manifests/neutron.pp
+++ deployment_scripts/puppet/manifests/neutron.pp
@@ -2,18 +2,18 @@ include onos
 
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 neutron_plugin_ml2 {
-  'ml2/mechanism_drivers':       value => 'onos';
+  'ml2/mechanism_drivers':       value => 'onos_ml2';
   'ml2/tenant_network_types':    value => 'vxlan';
-  'ml2_onos/password':           value => 'admin';
-  'ml2_onos/username':           value => 'admin';
+  'ml2_onos/password':           value => 'karaf';
+  'ml2_onos/username':           value => 'karaf';
   'ml2_onos/url_path':           value => "http://${onos::manager_ip}:8181/onos/vtn";
 }->
 
 exec{ 'Configure Neutron3':
         command  => "mysql -e 'drop database if exists neutron;';
-		    mysql -e 'create database neutron character set utf8;';
-		    mysql -e \"grant all on neutron.* to 'neutron'@'%';\";
-		    neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head;",
+                    mysql -e 'create database neutron character set utf8;';
+                    mysql -e \"grant all on neutron.* to 'neutron'@'%';\";
+                    neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head;",
 } ->
 exec{ 'restart neutron':
         command  => "service neutron-server restart",
