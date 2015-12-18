1039af666e63ca3ca19d0bf4e0ac67fcdbfea80a
dongfeng <albert.dongfeng@huawei.com>
2015-12-18 20:19:01 +0800

    modify net config
    Change-Id: I308db83c7c13d630920f5ed9b8293e86ddbf6cf3

---
+++ deployment_scripts/puppet/manifests/netconfig.pp
@@ -0,0 +1,19 @@
+
+Exec{
+        path => "/usr/bin:/usr/sbin:/bin:/sbin",
+        timeout => 180,
+        logoutput => "true",
+}
+
+$neutron_settings = hiera_hash('quantum_settings')
+$nets = $neutron_settings['predefined_networks']
+$gateway_ip = $nets['net04_ext']['L3']['gateway']
+
+file{ "/opt/netconfig.sh":
+        ensure => file,
+        content => template('onos/netconfig.sh.erb'),
+}->
+exec{ 'set gatewaymac':
+        command => "sh /opt/netconfig.sh;
+        rm -rf /opt/netconfig.sh;",
+}
