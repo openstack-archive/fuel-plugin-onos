b3326c5c3cec5c5f13db1e6bcafc9970850275cf
dongfeng <albert.dongfeng@huawei.com>
2016-01-18 21:54:31 -0500

    split delete into several steps
    Change-Id: I1b0cdb341fca1e68c47a33d7ad827efba9cc0555

--- deployment_scripts/puppet/manifests/neutron.pp
+++ deployment_scripts/puppet/manifests/neutron.pp
@@ -27,12 +27,21 @@ if member($roles, 'primary-controller') {
 cs_resource { 'p_neutron-l3-agent':
       ensure => absent,
 }->
-exec{ 'delete Neutron db':
-        command  => "mysql -e 'drop database if exists neutron;';
-                    mysql -e 'create database neutron character set utf8;';
-                    mysql -e \"grant all on neutron.* to 'neutron'@'%';\";
-                    neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head;",
-	before => Service ['start neutron service'],
+exec { 'drop_neutron_db':
+  command => "mysql -e 'drop database if exists neutron;'",
+}->
+
+exec { 'create_neutron_db':
+  command => "mysql -e 'create database neutron character set utf8;'",
+}->
+
+exec { 'grant_neutron_db':
+  command => "mysql -e \"grant all on neutron.* to 'neutron'@'%';\"",
+}->
+
+exec { 'neutron_db_sync':
+  command => 'neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head',
+  before => Service ['start neutron service'],
 }
 
 }
